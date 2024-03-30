11d63dc4a0f62ea2e6f3375323b4b4666e5cee8f
derpedcatto <sashavannovski@gmail.com>
2024-03-30 07:10:48 +0200

    Folder relocation

--- BnFurniture.Application/ExampleController/DTO/ExampleEntityFormDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BnFurniture.Application.ExampleController.DTO
-{
-    public class ExampleEntityFormDTO
-    {
-        public DateOnly Date { get; set; }
-        public int TemperatureC { get; set; }
-        public string? Summary { get; set; }
-    }
-}
